c4506755b378b9dbe8cfc72bd365cd3edacc8038
Sean Mooney <bable4@gmail.com>
2013-10-18 18:47:49 -0500

    Stubbing in required files.

---
+++ workflow/commit.py
@@ -0,0 +1,6 @@
+
+def main():
+    print("Hello git workflow commit")
+
+if __name__ == "__main__":
+    main()
---
+++ workflow/finish.py
@@ -0,0 +1,6 @@
+
+def main():
+    print("Hello git workflow finish")
+
+if __name__ == "__main__":
+    main()
